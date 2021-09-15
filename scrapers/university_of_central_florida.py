ce83026baa3c258835e4c2b2eff8ec72bc7e99fe
Warren Snipes <contact@warrensnipes.dev>
2021-09-15 03:13:35 -0400

    Added UCF implementation

--- scrapers/university_of_central_florida.py
+++ scrapers/university_of_central_florida.py
@@ -1,1 +1,32 @@
+import json
+
+import requests
+
+from course import Course
+
 def scrape_university_of_central_florida():
+    courses = []
+    # https://cdl.ucf.edu/support/student/modalities/
+    for mode in {"W", "V", "M", "RS", "P"}:
+        if len(courses) == 0:
+            courses = scrape_endpoint(mode)
+        else:
+            courses[0:0] = scrape_endpoint(mode)
+    return courses
+
+def scrape_endpoint(mode):
+    return parse(requests.get(
+        'https://cdl.ucf.edu/wp-content/themes/cdl/lib/course-search-ajax.php?call=classes&term=1710&prefix=&catalog=&title=&instructor=&career=&college=&department=&mode={}&_=1631687421296'.format(
+            mode)).content)
+
+
+def parse(data):
+    courses = []
+    decoded = json.loads(data)
+    for entry in decoded['classes']:
+        course = Course('', '', '')
+        course.crn = entry['registration_number']
+        course.course_code = '{}{}'.format(entry['prefix'], entry['catalog_number'])
+        course.instructor = '{} {}'.format(entry['name_first'], entry['name_last'])
+        courses.append(course)
+    return courses
---
+++ scrapers/university_of_central_florida_curl.sh
@@ -0,0 +1,15 @@
+curl 'https://cdl.ucf.edu/wp-content/themes/cdl/lib/course-search-ajax.php?call=classes&term=1710&prefix=&catalog=&title=&instructor=&career=&college=&department=&mode=WW&_=1631687421296' \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36' \
+  -H 'Content-Type: application/json; charset=utf-8' \
+  -H 'Sec-GPC: 1' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Referer: https://cdl.ucf.edu/support/student/search-courses/' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  --compressed
